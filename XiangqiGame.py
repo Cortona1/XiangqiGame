be8a0fc5d555ec9d8f171ad73c96fbe1a16c2f63
Cortona1 <anthonycorton95@gmail.com>
2020-03-22 13:05:03 -0400

    moved my board key declaration out side of the for loop that seems like a terrible indent error on my part

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -74,8 +74,8 @@ class XiangqiGame:
             else:
                 self._board.append(["", "", "", "", "", "", "", "", ""])
 
-            self._board_key = {"a" : 0, "b" : 1, "c" : 2, "d" : 3, "e" : 4, "f" : 5, "g" : 6, "h" : 7, "i" : 8}
-            #the board key contains the corresponding columns for letters
+        self._board_key = {"a" : 0, "b" : 1, "c" : 2, "d" : 3, "e" : 4, "f" : 5, "g" : 6, "h" : 7, "i" : 8}
+        #the board key contains the corresponding columns for letters
 
     def get_game_state(self):
         """Returns the state of the game"""
